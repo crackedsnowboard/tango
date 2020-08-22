54d1d5f8630d28aa2d1e338092a57eb4d56f073a
Prince of Kochi <mathenjs@gmail.com>
2020-08-22 12:28:57 -0700

    deleted readme to try and fix gh pages deployment

---
+++ build/precache-manifest.d811720e0dd7b18b21e491520802b819.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb70796f603ddb67d94c7f267403ab70",
+    "url": "/tango/index.html"
+  },
+  {
+    "revision": "6edadc344b5acac0d790",
+    "url": "/tango/static/css/main.6aca0fec.chunk.css"
+  },
+  {
+    "revision": "bcf24d996d986343ad12",
+    "url": "/tango/static/js/2.d6b06d5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tango/static/js/2.d6b06d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6edadc344b5acac0d790",
+    "url": "/tango/static/js/main.cd378f86.chunk.js"
+  },
+  {
+    "revision": "2c3589eb7904562a51a1",
+    "url": "/tango/static/js/runtime-main.60f46043.js"
+  },
+  {
+    "revision": "41ca296b3c6c98ee720089726412ee40",
+    "url": "/tango/static/media/headShot.41ca296b.png"
+  },
+  {
+    "revision": "b0f939e60ea6463d8aa6ed2643fe5737",
+    "url": "/tango/static/media/resume.b0f939e6.png"
+  }
+]);
